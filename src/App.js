cb1d7f1cba0b268b7961b6a098351ced8bf181c8
Phil <phil.shirley@iinteractive.com>
2017-08-25 13:49:39 -0700

    Initial commit to dev branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import xhr from 'xhr';
 
-class App extends Component {
+class App extends React.Component {
+  state = { 
+    location: '',
+    data: {}
+  };
+    
+  fetchData = (evt) => { 
+    evt.preventDefault();
+    var location = encodeURIComponent(this.state.location);
+
+    var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+    var urlSuffix = '&APPID=dc4714c5b142528708b3b032aaaf08d7&units=metric';
+    var url = urlPrefix + location + urlSuffix;
+
+    var self = this;
+
+    xhr({
+      url: url
+    }, function (err, data) {
+      self.setState({
+        data: JSON.parse(data.body)
+      });
+    });
+  };
+
+  changeLocation = (evt) => {
+    this.setState({
+      location: evt.target.value
+    });
+  };
+    
   render() {
+    var currentTemp = 'not loaded yet';
+    if (this.state.data.list) {
+      currentTemp = this.state.data.list[0].main.temp;
+    }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <h1>Weather</h1>
+        <form onSubmit={this.fetchData}>
+          <label> Current weather for 
+            <input placeholder={"City, Country"} 
+            type="text"
+            value={this.state.location}
+            onChange={this.changeLocation} 
+            />
+          </label>      
+        </form>
+        <p className="temp-wrapper">
+          <span className="temp">{ currentTemp }</span>
+          <span className="temp-symbol">°C</span>
+          </p>
+       </div> 
     );
   }
 }
